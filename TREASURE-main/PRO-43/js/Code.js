49f8597f085b97ba4b04237065e84475ba4f0365
pragya0310 <73340960+pragya0310@users.noreply.github.com>
2021-07-10 13:22:04 +0530

    Add files via upload

---
+++ TREASURE-main/PRO-43/js/Code.js
@@ -0,0 +1,26 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,80)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 90,50)
+
+
+fill("white")
+textSize(15)
+text("C U T F I O N", 400,290)
+fill("lightblue")
+text("Hint: Perform a particulat task !!", 380,270)
+
+fill("white")
+textSize(15)
+text("A T A D A S B A ", 750,180)
+fill("lightblue")
+text("Hint:store all the information !!", 720,160)
+
+}
